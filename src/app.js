458750a8d688a150afe9b332832527920c88f69a
duncan60 <tmduncan60@gmail.com>
2016-03-10 23:54:30 +0800

    init project

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import 'babel-polyfill';
+import React from 'react';
+import { render } from 'react-dom';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+
+import rootReducer from 'reducers';
+import appMiddleware from 'appMiddleware';
+
+import Root from 'components/containers/Root';
+
+
+const finalCreateStore = compose(
+    applyMiddleware(appMiddleware)
+)(createStore);
+
+const store = finalCreateStore(rootReducer);
+
+render(
+    <Provider store={store}>
+        <Root />
+    </Provider>,
+    document.getElementById('root')
+);
